e84e5d2ae5b3466a0c97b2253bd6183a9ff4226d
adarsh <aag70@cam.ac.uk>
2025-03-08 12:50:17 +0000

    this should maybe work for pulling data

--- extension/content.js
+++ extension/content.js
@@ -25,7 +25,63 @@ function fillForm(data) {
     console.log("Form filled successfully!");
 }
 
-// Listen for messages from popup
+
+function extractFormData() {
+    let formData = {
+        url: window.location.href,
+        title: document.title,
+        responses: []
+    };
+
+    let questions = document.querySelectorAll(".Qr70ae");
+    let inputs = document.querySelectorAll("input, textarea");
+
+    inputs.forEach((input, index) => {
+        let questionText = questions[index] ? questions[index].innerText : `Question ${index + 1}`;
+        let responseValue = "";
+
+        if (input.type === "radio" || input.type === "checkbox") {
+            if (input.checked) {
+                responseValue = input.nextSibling ? input.nextSibling.innerText : "Selected";
+            }
+        } else {
+            responseValue = input.value;
+        }
+
+        formData.responses.push({
+            question: questionText,
+            answer: responseValue
+        });
+    });
+
+    console.log("extracted form data: ", formData);
+    return formData;
+}
+
+function downloadJSON(data) {
+    let blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
+    let url = URL.createObjectURL(blob);
+
+    let downloadLink = document.createElement("a");
+    downloadLink.href = url;
+    downloadLink.download = "form_responses.json";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+}
+
+document.addEventListener("submit", (event) => {
+    let extractedData = extractFormData();
+    
+    downloadJSON(extractedData);
+
+    setTimeout(() => {
+        event.target.submit();
+    }, 500);
+});
+
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "fillForm") {
         fillForm(message.data);
