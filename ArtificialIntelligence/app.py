a27fdcb72aae817dc3b377ff80c30ccaeb05a4ac
Atharv-Nema <atharvnema12@gmail.com>
2025-03-09 16:56:47 +0000

    small change again

--- ArtificialIntelligence/app.py
+++ ArtificialIntelligence/app.py
@@ -89,8 +89,8 @@ def autofill():
 def addDatabase():
     data = request.get_json()
     print(f"Received data is {data}")
-    # jsonified_data = json.loads(data)
-    neo4j_db.update_user_profile(0, "Test profile", data)
+    jsonified_data = json.loads(data)
+    neo4j_db.update_user_profile(0, "Test profile", jsonified_data)
     return "Accepted"
 
 if __name__ == "__main__":
