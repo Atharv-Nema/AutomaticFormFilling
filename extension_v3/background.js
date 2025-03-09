fc1688370e38aa3624312ed5d4c740f8bd7218f4
Shih Ee <67913510+WhangShihEe@users.noreply.github.com>
2025-03-09 15:50:56 +0000

    added textbox

--- extension_v3/background.js
+++ extension_v3/background.js
@@ -1,6 +1,18 @@
+chrome.runtime.onStartup.addListener(function() {
+    console.log("Background script started!");
+  });
+
+  
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ["content.js"]
     });
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "saveData") {
+      const data = request.data;
+      console.log("Data saved:", data);
+    }
+  });
